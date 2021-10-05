e91cc4602400e13dd527b0d4c92eed0b82c77152
khmerdude <jett1004@gmail.com>
2021-10-05 12:52:14 -0400

    Park and Review Class added

---
+++ ReviewsSite/Models/Park.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Park
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool HasHandicapAccess { get; set; }
+        public bool DogFriendly { get; set; }
+
+        public ParkType ParkType { get; set; }
+    }
+    public enum ParkType
+    {
+        Museum,
+        Park,
+        Venue,
+        HistoricalSite
+    }
+}
---
+++ ReviewsSite/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Review
+    {
+        public virtual Park Park { get; set; }
+        public int ParkId { get; set; }
+        public string Comment { get; set; }
+        public string ReviewerName { get; set; }
+        public int Id { get; set; }
+        public int StarRating { get; set; }
+
+
+    }
+}
