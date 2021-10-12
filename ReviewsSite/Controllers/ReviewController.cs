8616085a0051ad6be88aab323dfaa11e9fde660b
khmerdude <jett1004@gmail.com>
2021-10-12 11:18:20 -0400

    Added Review Controller

---
+++ ReviewsSite/Controllers/ReviewController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ReviewsSite;
+using ReviewsSite.Models;
+
+namespace ReviewsSite.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly ParkContext _context;
+
+        public ReviewController(ParkContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Review
+        public async Task<IActionResult> Index()
+        {
+            var parkContext = _context.Reviews.Include(r => r.Park);
+            return View(await parkContext.ToListAsync());
+        }
+
+        // GET: Review/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.Park)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Review/Create
+        public IActionResult Create()
+        {
+            ViewData["ParkId"] = new SelectList(_context.Parks, "Id", "Id");
+            return View();
+        }
+
+        // POST: Review/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ParkId,ReviewerName,Comment,StarRating")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParkId"] = new SelectList(_context.Parks, "Id", "Id", review.ParkId);
+            return View(review);
+        }
+
+        // GET: Review/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["ParkId"] = new SelectList(_context.Parks, "Id", "Id", review.ParkId);
+            return View(review);
+        }
+
+        // POST: Review/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ParkId,ReviewerName,Comment,StarRating")] Review review)
+        {
+            if (id != review.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParkId"] = new SelectList(_context.Parks, "Id", "Id", review.ParkId);
+            return View(review);
+        }
+
+        // GET: Review/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.Park)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            _context.Reviews.Remove(review);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Reviews.Any(e => e.Id == id);
+        }
+    }
+}
