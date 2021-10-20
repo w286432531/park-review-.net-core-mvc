d427b96b391591c894e7df92c849288f7cbf390a
Jay Huang <53930607+w286432531@users.noreply.github.com>
2021-10-20 12:00:45 -0400

    add filter and search. add park orders. but order doesn't work after filter

--- ReviewsSite/Controllers/ParkController.cs
+++ ReviewsSite/Controllers/ParkController.cs
@@ -16,19 +16,55 @@ namespace ReviewsSite.Controllers
         {
             this._parkRepo = parkRepo;
         }
-        public IActionResult Index()
-        {
+        public IActionResult Index(string sortOrder, string searchString, bool isDogFriendly, bool hasHandicapAccess)
+        {   //add result from create park 
             if (TempData["Result"] != null)
             {
                 ViewBag.Result = TempData["Result"].ToString();
             }
 
             var parks = _parkRepo.GetAll();
-            foreach(var park in parks)
+            foreach (var park in parks)
             {
                 park.GetAverage();
             }
-
+            ViewBag.IdSortParm = String.IsNullOrEmpty(sortOrder)? "id_desc" : "";
+            ViewBag.IdSortParm = sortOrder == "id" ? "id_desc" : "id";
+            ViewBag.NameSortParm = sortOrder == "name" ? "name_desc" : "name";
+            ViewBag.RatingSortParm = sortOrder == "rating" ? "rating_desc" : "rating";
+            switch (sortOrder)
+            {
+                case "id_desc":
+                    parks = parks.OrderByDescending(park => park.Id);
+                    break;
+                case "name":
+                    parks = parks.OrderBy(park => park.Name);
+                    break;
+                case "name_desc":
+                    parks = parks.OrderByDescending(park => park.Name);
+                    break;
+                case "rating":
+                    parks = parks.OrderBy(park => park.AverageRating);
+                    break;
+                case "rating_desc":
+                    parks = parks.OrderByDescending(park => park.AverageRating);
+                    break;
+                default:
+                    parks = parks.OrderBy(park => park.Id);
+                    break;
+            }
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                parks = parks.Where(park => (park.Name.ToLower()).Contains(searchString.ToLower()));
+            }
+            if (isDogFriendly)
+            {
+                parks = parks.Where(park => park.IsDogFriendly);
+            }
+            if (hasHandicapAccess)
+            {
+                parks = parks.Where(park => park.HasHandicapAccess);
+            }
             return View(parks);
         }
 
@@ -49,17 +85,17 @@ namespace ReviewsSite.Controllers
             {
                 ViewBag.Error = "There is a error please try again.";
                 return View();
-            }           
+            }
         }
 
         public IActionResult Delete(int id)
         {
             Park park = _parkRepo.GetByID(id);
-           
+
             return View(park);
         }
         [HttpPost]
-        public IActionResult Delete(int id, Park park) 
+        public IActionResult Delete(int id, Park park)
         {
             Park parkDel = _parkRepo.GetByID(park.Id);
             _parkRepo.Delete(parkDel);
@@ -94,7 +130,7 @@ namespace ReviewsSite.Controllers
             return RedirectToAction("Index");
 
         }
- 
+
 
 
 
